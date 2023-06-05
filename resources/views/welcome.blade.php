c5901ad1ddd0ac869dd02c0c61c32f60f51aab75
betomendezleite <bmlaudiovisuales@gmail.com>
2023-06-05 00:43:02 -0400

    'login_sreen'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+        <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body class="bgdarkgradient vh-100 d-flex align-items-center justify-content-center">
